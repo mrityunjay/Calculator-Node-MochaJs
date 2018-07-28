759e98314c8c286baa74f588cae869d10b6d4cc5
Mrityunjay Kumar Ranjan <mrityunjaykumarranjan@mriranja-mac.local>
2018-07-28 14:06:49 +0530

    improved the calc code and added more test.

--- calc.js
+++ calc.js
@@ -1,5 +1,8 @@
 module.exports = {
 	my_calculator: function(operator, value1, value2){
+		if(typeof value1 != 'number' || typeof value2 != 'number'){
+			return "Operation Not possible with other than number"
+		}
 		if(operator=='+'){
 			return value1+value2;
 		}else if(operator=='-'){
--- test/calcTest.js
+++ test/calcTest.js
@@ -6,13 +6,31 @@ describe('App', function(){
 		let result=calc('+',5,5);
 		assert.equal(result,10);
 	});
+	it('verify add functionality with max save integer', function(){
+		let result=calc('+',Number.MAX_SAFE_INTEGER,Number.MAX_SAFE_INTEGER);
+		assert.equal(result,18014398509481982);
+	});
+	it('verify negative number addition', function(){
+		assert.equal(calc('+',10,-20),-10);
+	});
 	it('verify substract functionality', function(){
 		let result=calc('-',5,5);
 		assert.equal(result,0);
 	});
+	it('verify substraction with negative number', function(){
+		assert.equal(calc('-', -4, -3),-1);
+	});
 	it('verify multiply functionality', function(){
 		let result=calc('*',5,5);
 		assert.equal(result,25);
+	});
+	it('verify multiply functionality with 0', function(){
+		let result=calc('*',5,0);
+		assert.equal(result,0);
+	});
+	it('verify multiply functionality using MAX_SAFE_INTEGER', function(){
+		let result=calc('*',Number.MAX_SAFE_INTEGER,Number.MAX_SAFE_INTEGER);
+		assert.equal(result,8.112963841460666e+31);
 	});
 	it('verify divide functionality', function(){
 		let result=calc('/',5,5);
@@ -22,4 +40,9 @@ describe('App', function(){
 		let result=calc('/',5,0);
 		assert.equal(result,"You can't devide number by 0");
 	});
+	it('test addition with non number data', function(){
+		let result=calc('+',"5","6");
+		assert.equal(result, "Operation Not possible with other than number");
+	});
+
 });
